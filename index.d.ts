1103949b2a65d37eb94cd8166d0111936c44e248
Armagan Amcalar <armagan@amcalar.com>
2022-04-03 17:32:26 +0200

    Fix type definition for multiple backends

--- index.d.ts
+++ index.d.ts
@@ -35,61 +35,82 @@ interface RoleParentsDocument extends RoleParents, Document {}
 interface AccessRuleDocument extends AccessRule, Document {}
 interface UserDocument extends User, Document {}
 
-export function addUserRoles(userId: string, roles: string | string[]): Promise<UserDocument>
+type hakki = {
+  addUserRoles(userId: string, roles: string | string[]): Promise<UserDocument>
 
-export function removeUserRoles(userId: string, roles: string | string[]): Promise<void>
+  removeUserRoles(userId: string, roles: string | string[]): Promise<void>
 
-export function userRoles(userId: string): Promise<string[]>
+  userRoles(userId: string): Promise<string[]>
 
-export function roleUsers(role: string): Promise<string[]>
+  roleUsers(role: string): Promise<string[]>
 
-export function hasRole(userId: string, role: string | string[]): Promise<boolean>
+  hasRole(userId: string, role: string | string[]): Promise<boolean>
 
-export function addRoleParents(role: string, parents: string | string[]): Promise<RoleParentsDocument>
+  addRoleParents(
+    role: string,
+    parents: string | string[]
+  ): Promise<RoleParentsDocument>
 
-export function removeRoleParents(role: string): Promise<RemovalResponse>
-export function removeRoleParents(role: string, parents?: string | string[]): Promise<RoleParentsDocument>
+  removeRoleParents(role: string): Promise<RemovalResponse>
 
-export function removeRole(role: string): Promise<void>
+  removeRoleParents(
+    role: string,
+    parents?: string | string[]
+  ): Promise<RoleParentsDocument>
 
-export function removeResource(resource: string): Promise<void>
+  removeRole(role: string): Promise<void>
 
-export function allow(
-  bulkAccessRuleRequest: BulkAccessRuleRequest | BulkAccessRuleRequest[]
-): Promise<AccessRuleDocument[]>
+  removeResource(resource: string): Promise<void>
 
-export function allow(
-  roles: string | string[],
-  resources: string | string[],
-  permissions: string | string[]
-): Promise<AccessRuleDocument[]>
+  allow(
+    bulkAccessRuleRequest: BulkAccessRuleRequest | BulkAccessRuleRequest[]
+  ): Promise<AccessRuleDocument[]>
 
-export function removeAllow(
-  roles: string | string[],
-  resources: string | string[],
-  permissions: string | string[]
-): Promise<RemovalResponse[]>
+  allow(
+    roles: string | string[],
+    resources: string | string[],
+    permissions: string | string[]
+  ): Promise<AccessRuleDocument[]>
 
-export function allowedPermissions<T extends string, U = { [K in T]: string[] }>(
-  userId: string,
-  resources: T | T[]
-): Promise<U>
+  removeAllow(
+    roles: string | string[],
+    resources: string | string[],
+    permissions: string | string[]
+  ): Promise<RemovalResponse[]>
 
-export function isAllowed(userId: string, resource: string, permissions: string | string[]): Promise<boolean>
+  allowedPermissions<T extends string, U = { [K in T]: string[] }>(
+    userId: string,
+    resources: T | T[]
+  ): Promise<U>
 
-export function areAnyRolesAllowed(roles: string[], resource: string, permissions: string[]): Promise<boolean>
+  isAllowed(
+    userId: string,
+    resource: string,
+    permissions: string | string[]
+  ): Promise<boolean>
 
-export function whatResources(
-  role: string | string[],
-  permissions: string | string[]
-): Promise<string[] | Record<string, string[]>>
+  areAnyRolesAllowed(
+    roles: string[],
+    resource: string,
+    permissions: string[]
+  ): Promise<boolean>
 
-export function isRole(userId: string, role: string): Promise<boolean>
+  whatResources(
+    role: string | string[],
+    permissions: string | string[]
+  ): Promise<string[] | Record<string, string[]>>
 
-export function getDistinctRoles(): Promise<string[]>
+  isRole(userId: string, role: string): Promise<boolean>
 
-export function getDistinctPermissions(): Promise<string[]>
+  getDistinctRoles(): Promise<string[]>
 
-export function roleUsersWithParentRoles(role: string): Promise<string[]>
+  getDistinctPermissions(): Promise<string[]>
 
-export function roleUsersIncludingInheritedRoles(role: string | string[]): Promise<string[]>
+  roleUsersWithParentRoles(role: string): Promise<string[]>
+
+  roleUsersIncludingInheritedRoles(role: string | string[]): Promise<string[]>
+}
+
+declare const module: hakki & ((opts) => hakki)
+
+export default module
